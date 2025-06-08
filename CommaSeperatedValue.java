f211b1b1142d785934a232e4200d163289d2ed4c
unknown <dipakmmali3@gmail.com>
2025-06-08 22:59:04 +0530

    INR functionality added

--- CommaSeperatedValue.java
+++ CommaSeperatedValue.java
@@ -24,8 +24,38 @@ public class CommaSeperatedValue {
             System.out.println("Number with more than 3 digits: " + "$"+result);
         }
     }
+
+    public static void inrCommmaSeparated(String number) {
+
+        String result = "";
+        int count = 0;
+        int numSize = (int)number.length();
+
+        System.out.println("INR Number length: "+ numSize);
+
+        if(numSize <= 3) {
+            System.out.println("Number with less than 3 digits: "+number);
+        }
+
+        if(numSize > 3) {
+            for(int i=number.length() - 1; i >= 0; i--) {
+                result = number.charAt(i) + result;
+                count++;
+
+                if(count == 3) {
+                    result = "," + result;
+                }
+
+                if(count > 3 && count % 2 != 0 && i != 0) {
+                    result = "," + result;
+                }
+            }
+            System.out.println("Number with more than 3 digits: "+result);
+        }
+    }
     
     public static void main(String[] args) {
         commaSeperatedNumber("110698");
+        inrCommmaSeparated("123456789");
     }
 }
